3a11ec5e14498c75b416d348b046d349f90e69b3
hide24 <hidetoshi.yoshimoto@gmail.com>
2020-10-05 23:15:52 +0900

    add ldap auth

--- app/javascript/components/MenuLink.vue
+++ app/javascript/components/MenuLink.vue
@@ -23,11 +23,6 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
 
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
           <b-nav-item-dropdown :text="t('layouts.navi_bar.languages')" right>
             <b-dropdown-item href="#" @click="changeLocale('en')">{{ t('layouts.navi_bar.english') }}</b-dropdown-item>
             <b-dropdown-item href="#" @click="changeLocale('ja')">{{ t('layouts.navi_bar.japanese') }}</b-dropdown-item>
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :certificate_applications
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+  devise :ldap_authenticatable
+#  devise :database_authenticatable, :registerable,
+#         :rememberable, :validatable
 end
